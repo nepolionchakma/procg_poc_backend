bfc911b52ab94463d42191f7bdea1d5ff029afd7
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-07-09 14:43:22 +0600

    rename route

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,5 @@
 import tenantsRoutes from "./def_tenantRoute";
 import usersRoutes from "./def_usersRoute";
 import personsRoutes from "./def_personsRoute";
-import user_credentialsRoutes from "./def_user_credentials";
+import user_credentialsRoutes from "./def_user_credentialsRoute";
 export { tenantsRoutes, usersRoutes, personsRoutes, user_credentialsRoutes };
